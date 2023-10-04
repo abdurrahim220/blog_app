f14a0a08c6b8d9d0fb21e96dd67fbb9aa375b5d7
abdurrahim220 <abdurrahim5@gmail.com>
2023-10-04 16:36:36 +0600

    NavBar is created

--- src/component/NavBar/NavBar.jsx
+++ src/component/NavBar/NavBar.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { BsSearch } from "react-icons/bs";
 
 const NavBar = () => {
+  const user = false;
   return (
-    <div>NavBar</div>
-  )
-}
+    <div className="flex justify-between items-center px-6 md:px-[200px] py-4">
+      <div className="text-lg md:text-xl font-semibold">
+        <Link to="/">Blog Market</Link>
+      </div>
 
-export default NavBar
+      <div className="flex justify-center items-center space-x-0">
+        <p>
+          <BsSearch />
+        </p>
+        <input
+          type="text"
+          placeholder="Search a post"
+          className="outline-none px-3"
+        />
+      </div>
+
+      <div className="flex items-center justify-center space-x-2 md:space-x-4">
+        {user ? (
+          <li>
+            <Link to="/write">Write</Link>
+          </li>
+        ) : (
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        )}
+
+        {user ? (
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+        ) : (
+          <li>
+            <Link to="login">Register</Link>
+          </li>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
